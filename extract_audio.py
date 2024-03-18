0ed8103c9f72095236048287b1872f72de50c2b5
JiaXian Tan <jack1093562028@gmail.com>
2024-03-19 07:33:56 +0800

    trail a ok

--- extract_audio.py
+++ extract_audio.py
@@ -1,33 +1,17 @@
-#
-# from moviepy.editor import VideoFileClip
-#
-#
-# # 加载视频文件
-# video_file = "./data/1435937490-1-192.mp4"
-# video = VideoFileClip(video_file)
-#
-# # 提取音频
-# audio = video.audio
-#
-# # 写入音频文件
-# audio_file = "./data/1435937490-1-192.wav"
-# audio.write_audiofile(audio_file)
-#
-# # 关闭视频文件
-# video.close()
 
-import yt_dlp
-yt_url = 'https://www.youtube.com/watch?v=tgdJkAx3fJM'
-ydl_opts = {
-    'format': 'm4a/bestaudio/best',
-    'outtmpl': '%(id)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'wav',
-    }],
-}
+from moviepy.editor import VideoFileClip
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    error_code = ydl.download(yt_url)
-    video_info = ydl.extract_info(yt_url, download=False)
-    file_name = f"{video_info['id']}.wav"
+
+# 加载视频文件
+video_file = "./data/1429275928-1-192.mp4"
+video = VideoFileClip(video_file)
+
+# 提取音频
+audio = video.audio
+
+# 写入音频文件
+audio_file = "./data/audio_16k.wav"
+audio.write_audiofile(audio_file)
+
+# 关闭视频文件
+video.close()
--- separate.py
+++ separate.py
@@ -1,9 +1,13 @@
 import os
 import demucs.separate
 import shlex
+import glob
+
+file_list = glob.glob(os.path.join('data', '*.wav'))
 
 file_extension = ['.mp4', '.wav']
-allowed_files = [file for file in os.listdir() if any(file.lower().endswith(ext) for ext in file_extension)]
+allowed_files = [file for file in file_list if any(file.lower().endswith(ext) for ext in file_extension)]
+print(allowed_files)
 input_file = max(allowed_files, key=lambda file: os.path.getctime(file))
 
 demucs.separate.main(shlex.split(f'-n htdemucs --two-stems=vocals "{input_file}" -o "temp_outputs"'))
